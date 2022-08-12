65c9e91ea0d0621dff7885685d77b197770e8c52
mcasali <50184471+mcasali@users.noreply.github.com>
2022-08-12 10:48:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def get_centroid(gage_id_geo):
     df = gpd.read_file("./Data/Geojsons/{}.geojson".format(gage_id_geo))
     st.session_state.zoom_center_y = float(df["geometry"].centroid.x)
     st.session_state.zoom_center_x = float(df["geometry"].centroid.y) - 0.075
-    st.session_state.zoom_level = 11
+    st.session_state.zoom_level = 12
     return df
 
 
