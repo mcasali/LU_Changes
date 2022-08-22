609d4109612ced9e01b6596ecaedca18f1b44842
mcasali <50184471+mcasali@users.noreply.github.com>
2022-08-22 11:30:47 -0600

    Updated mapping package

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import geopandas as gpd
 import pandas as pd
-import leafmap
+import geemap.foliumap as geemap
 from PIL import Image
 
 # Set wide mode
@@ -32,16 +32,30 @@ def get_centroid(gage_id_geo):
 def show_data(gage_id):
     if gage_id == "All":
         df = gpd.read_file("./Data/Geojsons/{}.geojson".format(gage_id))
-        main_map = leafmap.Map(center=(33, -96), zoom=4, draw_control=False, measure_control=False, google_map="HYBRID")
-        main_map.add_gdf(df, layer_name=gage_id, zoom_to_layer=False, fill_colors=['blue'])
-        main_map.to_streamlit(responsive=True)
+        m = geemap.Map(
+            center=(33, -96),
+            zoom=4,
+            basemap="HYBRID",
+            plugin_Draw=True,
+            Draw_export=True,
+            locate_control=True,
+            plugin_LatLngPopup=False,
+        )
+        m.add_gdf(df, layer_name=gage_id, zoom_to_layer=False, fill_colors=['blue'])
+        m.to_streamlit(responsive=True)
     else:
         df2 = get_centroid(gage_id)
-        main_map = leafmap.Map(center=(st.session_state.zoom_center_x, st.session_state.zoom_center_y),
-                               zoom=st.session_state.zoom_level, draw_control=False, measure_control=False,
-                               google_map="HYBRID")
-        main_map.add_gdf(df2, layer_name=gage_id, zoom_to_layer=False, fill_colors=['blue'])
-        main_map.to_streamlit(responsive=True)
+        m = geemap.Map(
+            center=(st.session_state.zoom_center_x, st.session_state.zoom_center_y),
+            zoom=st.session_state.zoom_level,
+            basemap="HYBRID",
+            plugin_Draw=True,
+            Draw_export=True,
+            locate_control=True,
+            plugin_LatLngPopup=False,
+        )
+        m.add_gdf(df2, layer_name=gage_id, zoom_to_layer=False, fill_colors=['blue'])
+        m.to_streamlit(responsive=True)
 
         col1, col2 = st.columns(2)
         with col1:
